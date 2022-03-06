cba01ef59f259bfa6337d39782b90a32b5d55916
Bill Martin <bill@billmartin.io>
2022-03-05 23:37:31 -0500

    Create dotfiles setup.sh, and restructure .dotfiles/

---
+++ .dotfiles/setup.sh
@@ -0,0 +1,40 @@
+
+
+# Symlink these dot files to the ones in this repo 
+printf "# Setting up dot file symlinks..\n"
+
+# For each dot file in dotfiles
+for FILE in .[^.]*; do
+    # If it exists in ~/ already
+    if [[ -f ~/$FILE ]]; then
+        printf "\n~/${FILE} exists.\n"
+        printf "Consider running: diff ~/${FILE} ~/.dotfiles/${FILE}\n"
+        read -p "Overwrite ~/${FILE}? y/n: " yn
+
+        if [[ $yn == "y" ]]; then
+            rm ~/.bashrc && ln -s ~/.dotfiles/.bashrc ~/.bashrc
+            printf "Symlinked: ~/${FILE} -> ~/.dotfiles/${FILE}\n"
+        else
+            printf "\nSkipping file..\n"
+        fi
+    fi
+done
+
+printf "\n# Including dotfiles in your ~/.bashrc..."
+if [[ $(cat ~/.bashrc | grep dotfiles) ]]; then
+    printf "\nThe dotfile include is already present in ~/.bashrc. No action taken.\n"
+else
+    cat >> ~/.bashrc <<'endmsg'
+
+# Load .bashrc customizations and aliases
+if [ -f ~/.dotfiles/bashrc-includes/include ]; then
+   . ~/.dotfiles/bashrc-includes/include
+fi
+
+endmsg
+    printf "\nAdded include for dotfile aliases/config in your ~/.bashrc\n"
+fi 
+
+printf "\n# Next Steps: \n"
+printf "\n- Refresh bash: . ~/.bashrc"
+printf "\n- View aliases: a\n"
