f47f327b6200840e9c76541202fa9fab22aae302
Bill Martin <bill@billmartin.io>
2022-03-07 21:42:24 -0500

    Improve setup.sh with color-coded output, and to check for overwriting directories

--- setup.sh
+++ setup.sh
@@ -4,36 +4,52 @@
 cd $(dirname $0)
 
 # Symlink these dot files to the ones in this repo 
-printf "# Setting up dot file symlinks..\n"
+printf "Setting up dot file symlinks...\n"
 
 # For each dot file in dotfiles
 for FILE in .[^.]*; do
 	
+    # Do not symlink this repo's .git file
     if [[ $FILE == ".git" ]]; then
         continue
     fi	    
 
     # If it exists in ~/ already
-    if [[ -f ~/$FILE ]]; then
-        printf "\n~/${FILE} exists.\n"
-        printf "Consider running: diff ~/${FILE} ~/dot-files/${FILE}\n"
-        read -p "Overwrite ~/${FILE}? y/n: " yn
+    if [[ -f ~/$FILE || -d ~/$FILE ]]; then
+        # Show warning for files about to get overwritten
+        if [[ -f ~/$FILE ]]; then
+            echo -e "\n  \e[33m~/${FILE} exists.\e[0m"
+            echo -e "  \e[33mConsider running: diff ~/${FILE} ~/dot-files/${FILE}\e[0m"
+            read -p "  Overwrite ~/${FILE}/? y/n: " yn
+        fi
+        
+        # Show warning for directories about to get overwritten
+        if [[ -d ~/$FILE ]]; then
+            echo -e "\n  \e[33m~/${FILE}/ exists, and is a directory.\e[0m"
+            echo -e "  \e[33m~/Make sure file you wan to keep from ~/${FILE}/ is in ~/dot-files/${FILE}/ otherwise you are about to lose them!/\e[0m"
+            read -p "  Overwrite ~/${FILE}? y/n: " yn
+        fi
 
         if [[ $yn == "y" ]]; then
-            rm ~/$FILE && ln -s ~/dot-files/$FILE ~/$FILE
-            printf "Symlinked: ~/${FILE} -> ~/dot-files/${FILE}\n"
+            rm -r ~/$FILE && ln -s ~/dot-files/$FILE ~/$FILE
+            echo -e "  \e[32mSymlinked: ~/${FILE} -> ~/dot-files/${FILE}\e[0m"
         else
-            printf "\nSkipping file..\n"
+            echo -e "  \e[32mSkipping file...\e[0m"
         fi
     else
         ln -s ~/dot-files/$FILE ~/$FILE
-        printf "Symlinked: ~/${FILE} -> ~/dot-files/${FILE}\n"
+        echo -e "  \e[32mSymlinked: ~/${FILE} -> ~/dot-files/${FILE}\e[0m"
     fi
 done
 
-printf "\n# Including dotfiles in your ~/.bashrc..."
+printf "\nIncluding dotfiles in your ~/.bashrc...\n"
+
+# if the below block is already in .bashrc
 if [[ $(cat ~/.bashrc | grep dot-files) ]]; then
-    printf "\nThe dotfile include is already present in ~/.bashrc. No action taken.\n"
+    echo -e "\n  \e[32mYour aliases and bashrc customizations are already included in your ~/.bashrc.\e[0m"
+    echo -e "  \e[32mNo action taken.\e[0m"
+
+# else include the below block in .bashrc
 else
     cat >> ~/.bashrc <<'endmsg'
 
@@ -43,9 +59,11 @@ if [ -f ~/dot-files/includes/include ]; then
 fi
 
 endmsg
-    printf "\nAdded include for dotfile aliases/config in your ~/.bashrc\n"
+
+    echo -e "\n  \e[32mAdded include for ~/dot-files/includes/include in your ~/.bashrc.\e[0m"
+    echo -e "  \e[32mAliases and bash customizations loaded.\e[0m"
 fi 
 
-printf "\n# Next Steps: \n"
-printf "\n- Refresh bash: . ~/.bashrc"
-printf "\n- View aliases: a\n"
+printf "\nNext Steps: \n"
+printf "\n  - Refresh bash: . ~/.bashrc"
+printf "\n  - View aliases: a\n"
